919c7390f51cafceb84eab7224ec8b8e5291caef
RafaXH <33483219+RafaXH@users.noreply.github.com>
2018-02-22 09:54:37 +0000

    Update Pessoa.java

--- pl01/src/main/java/com/diogocapela/pl01/Pessoa.java
+++ pl01/src/main/java/com/diogocapela/pl01/Pessoa.java
@@ -1,41 +1,84 @@
-package com.diogocapela.pl01;
-
 public class Pessoa {
-    
-    private String nome;
-    private int idade;
-    
+    //
+    // class object attributes
+    //
+    String nome;
+    int idade;
+
+    //class constants
+    private static final String DEFAULT_NOME = "";
+    private static final int DEFAULT_IDADE = 0;
+
+    //class variables
+
+    //
+    // constructors
+    //
     public Pessoa() {
-        this.nome = "";
+        setNome(DEFAULT_NOME);
+        setIdade(DEFAULT_IDADE);
     }
-    
+
     public Pessoa(String nome) {
-        this.nome = nome;
+        setNome(nome);
+        setIdade(0);
     }
-    
+
     public Pessoa(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
+        setNome(nome);
+        setIdade(idade);
     }
-    
+
+    //
+    // getters
+    //
     public String getNome() {
         return this.nome;
     }
-    
+
     public int getIdade() {
         return this.idade;
     }
-    
-    public void setNome(String nome) {
-        this.nome = nome;
+
+    //
+    // setters
+    //
+    public void setNome(String novoNome) {
+        if (novoNome.length() > 0) {
+            this.nome = novoNome;
+        } else {
+            this.nome = "";
+        }
     }
-    
-    public void setIdade(int idade) {
-       this.idade = idade;
+
+    public void setIdade(int novaIdade) {
+        if (novaIdade >= 0) {
+            this.idade = novaIdade;
+        }
     }
-    
-    public String toString() {
-        return "O nome é ${this.nome}\nA idade é ${this.idade}";
+
+    //
+    // methods
+    //
+    public String toString2() {
+        if (this.nome.length() > 0) {
+            return "O meu nome é " + this.nome + ".";
+        } else {
+            return "Não sei o meu nome.";
+        }
     }
-    
+
+    public String toString3() {
+        if (this.nome.length() > 0 && this.idade > 0 ) {
+            return "O meu nome é " + this.nome + " e a minha idade é " + this.idade;
+        } else if (this.nome.length() > 0 && this.idade == 0) {
+            return "O meu nome é " + this.nome + " e não sei a minha idade.";
+        } else {
+            return "Não sei nem o meu nome nem a minha idade";
+        }
+    }
+
+    public void cumprimentar(String nome) {
+        System.out.println("Olá " + nome + "!");
+    }
 }
