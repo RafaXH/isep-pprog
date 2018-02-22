a7a52b1fa9da04b4e9572c3fb515396659f41659
RafaXH <33483219+RafaXH@users.noreply.github.com>
2018-02-22 10:18:54 +0000

    Add files via upload

---
+++ TP01/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Automovel a1 = new Automovel("11-11-AA","Toyota", 1400);
+
+        //a1.toString();
+
+        System.out.print(a1.toString());
+    }
+}
