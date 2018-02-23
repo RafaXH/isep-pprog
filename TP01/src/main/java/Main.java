017c3032a9e78b575eda5b4096305f9f8feb496d
RafaXH <33483219+RafaXH@users.noreply.github.com>
2018-02-23 16:23:51 +0000

    Add files via upload

--- TP01/src/main/java/Main.java
+++ TP01/src/main/java/Main.java
@@ -1,12 +1,58 @@
 public class Main {
-
     public static void main(String[] args) {
-
-
+        //
+        // 1
+        //
         Automovel a1 = new Automovel("11-11-AA","Toyota", 1400);
-
-        //a1.toString();
-
-        System.out.print(a1.toString());
+        //
+        // 2
+        //
+        System.out.println(a1.toString());
+        //
+        // 3
+        //
+        System.out.println("A matrícula é " + a1.getMatricula());
+        //
+        // 4
+        //
+        System.out.println("O total de automóveis é " + Automovel.getTotalAutomoveis());
+        //
+        // 5
+        //
+        Automovel a2 = new Automovel();
+        //
+        // 6
+        //
+        a2.setCilindrada(1800);
+        //
+        // 7
+        //
+        System.out.println(a2.toString());
+        //
+        // 8
+        //
+        System.out.println("O total de automóveis é " + Automovel.getTotalAutomoveis());
+        //
+        // 9
+        //
+        System.out.println("A diferença de cilindrada entre a1 e a2 é " + a2.calcularDiferencaCilindrada(a1));
+        //
+        // 10
+        //
+        if (a1.isCilindradaMaior(a2)) {
+            System.out.println("A cilindrada de a1 é maior que a de a2");
+        } else if (a2.isCilindradaMaior(a1)) {
+            System.out.println("A cilindrada de a2 é maior que a de a2");
+        } else {
+            System.out.println("Os dois carros têm a mesma cilindrada");
+        }
+        //
+        // 11
+        //
+        if (a1.isCilindradaMaior(2000)) {
+            System.out.println("A cilindrada de a1 é maior que 2000c");
+        } else {
+            System.out.println("A cilindrada de a1 não é maior que 2000c");
+        }
     }
 }
